832ac7c79267aaca515714d86f735757ef5f0c7b
Anton Danilov <antondanilov@MacBook-Air-Anton.local>
2025-10-02 16:56:25 +0200

    Initial Commit

---
+++ EggBuck Pins/EggBuck_PinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggBuck_PinsApp.swift
+//  EggBuck Pins
+//
+//  Created by Anton Danilov on 30/9/25.
+//
+
+import SwiftUI
+//
+//@main
+//struct EggBuck_PinsApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
